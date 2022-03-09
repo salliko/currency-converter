3d00cda617f8020b7755103c647e9cb40eecd146
grebennikov_iy <grebennikov_iy@magnit.ru>
2022-03-09 23:26:34 +0300

    Убрал не актуальный тест

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,12 +1,1 @@
-from app.main import app
 
-import pytest
-
-from starlette.testclient import TestClient
-
-
-@pytest.fixture(scope='module')
-def test_app():
-    """Функция возвращает тестовый клиент."""
-    client = TestClient(app)
-    yield client
